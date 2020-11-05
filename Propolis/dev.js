2296ac2fd8a637402d7a5a657a24ac2eb2d9c17c
yangseungbin <tanja1206@naver.com>
2020-11-05 21:20:40 +0900

    [Level_2] 기능개발

--- Propolis/dev.js
+++ Propolis/dev.js
@@ -1,17 +1,21 @@
 function solution(progresses, speeds) {
-  var answer = new Array(100).fill(0);
-  let arr = new Array(progresses.length).fill(0);
-  for (let i = 0; i < progresses.length; i++) {
-    while (1) {
-      progresses[i] += speeds[i];
-      arr[i]++;
-      if (progresses[i] >= 100) {
-        answer[arr[i]]++;
-        break;
+  var answer = [];
+
+  while (progresses.length > 0) {
+    for (let i = 0; i < speeds.length; i++) {
+      if (progresses[i] < 100) {
+        progresses[i] += speeds[i];
       }
     }
+    let count = 0;
+    while (progresses[0] >= 100) {
+      progresses.shift();
+      speeds.shift();
+      count++;
+    }
+    if (count > 0) {
+      answer.push(count);
+    }
   }
-  return answer.filter((e) => e != 0);
+  return answer;
 }
-
-console.log(solution([93, 30, 55], [1, 30, 5]));
