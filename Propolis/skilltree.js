863d896c37e3fb13f7dcbc528d99f465beb6064b
yangseungbin <tanja1206@naver.com>
2020-11-10 22:28:43 +0900

    [Level_2] 기능개발 짧은 풀이

--- Propolis/skilltree.js
+++ Propolis/skilltree.js
@@ -13,3 +13,21 @@ function solution(skill, skill_trees) {
   });
   return answer;
 }
+
+// filter : 말 그대로 필터. 콜백 안의 조건을 만족하는 애들만 필터링해서 갖고옴
+// indexOf : 배열 내 특정 요소의 인덱스 가져옴
+// split : 문자열을 배열로 분리
+// join : 배열을 문자열로 합침
+// length : 배열 or 문자열의 길이
+
+// function solution(skill, skill_trees) {
+//   return skill_trees.filter(
+//     (tree) =>
+//       skill.indexOf(
+//         tree
+//           .split("")
+//           .filter((s) => skill.split("").includes(s))
+//           .join("")
+//       ) === 0
+//   ).length;
+// }
