1025e46bcedb2d606359f8c0d817980357d08714
yangseungbin <tanja1206@naver.com>
2020-12-10 21:10:06 +0900

    [Level_3] [1차] 추석트래픽

--- Propolis/traffic.js
+++ Propolis/traffic.js
@@ -13,10 +13,7 @@ function solution(lines) {
       if (l[1] >= line[0] && l[0] <= line[0] + 999) cnt[0]++;
       if (l[1] >= line[1] && l[0] <= line[1] + 999) cnt[1]++;
     }
-    console.log(cnt);
     answer = Math.max(...cnt, answer);
   }
-
-  console.log(newLines);
   return answer;
 }
